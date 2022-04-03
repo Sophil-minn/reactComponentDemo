658965817c96c7b673b878da7e0e7309effb8b1c
minmin <1056640663@qq.com>
2022-04-03 18:30:18 +0800

    feat: 用StaggeredMotion 实现一个联动动画，链式反应的效果

--- src/pages/motion/index.jsx
+++ src/pages/motion/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Motion, spring, presets} from 'react-motion'
+import {Motion, spring, presets, StaggeredMotion } from 'react-motion'
 import './index.css';
 
 // 相比较react-transition-group，用react-motion进行动画的编写显得要直观很多，写法类似这样
@@ -51,5 +51,73 @@ class Test1 extends Component {
   )
 }
 }
+class Test2 extends Component {
+  state = {
+    length: 10
+  }
 
-export default Test1;
+  addLength() {
+    let newLength
+    if (this.state.length) {
+      newLength = 0
+    } else {
+      newLength = 10
+    }
+
+    this.setState({
+      length: newLength
+    })
+  }
+
+  render() {
+    let boxes = []
+    for (let i = 0, len = this.state.length; i < len; i++) {
+      boxes.push({
+        scale: 0
+      })
+    }
+
+    return (
+      <div>
+        <div>
+          {this.state.length > 0 ? (
+            <StaggeredMotion defaultStyles={boxes}
+              styles={prevStyles => prevStyles.map((item, i) => {
+                return i === 0
+                  ? { scale: spring(1, { ...presets.noWobble }) }
+                  : prevStyles[i - 1]
+              })}>
+              {interpolatingStyles =>
+                <div>
+                  {interpolatingStyles.map((item, i) => {
+                    return (
+                      <div className="box box2"
+                        key={i}
+                        style={{
+                          transform: `scale(${item.scale}, ${item.scale})`
+                        }}></div>
+                    )
+                  })}
+                </div>
+              }
+            </StaggeredMotion>
+          ) : null}
+        </div>
+        <button onClick={this.addLength.bind(this)}>run</button>
+      </div>
+    )
+  }
+}
+
+
+const Index = () => {
+  return (
+    <div>
+      <Test1 />
+      <Test2 />
+      
+    </div>
+  );
+}
+
+export default Index;
